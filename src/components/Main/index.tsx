fc61d27c1da9de4d77104301144889f972d0e401
Frederico Berchof <fredericoberchof@Fredericos-MacBook-Air.local>
2022-08-08 16:40:53 -0300

    main e layout

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Profile } from 'styled-icons/icomoon';
+
+import { Container, Header, BackIcon, ProfileInfo  } from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <button>
+          <BackIcon />
+        </button>
+
+        <ProfileInfo>
+          <strong>Fred Berchof</strong>
+          <span>600 Tweets</span>
+        </ProfileInfo>
+      </Header>
+
+      {/* <ProfilePage /> */}
+
+      {/* <BottomMenu>
+        <HomeIcon />
+        <SearchIcon />
+        <BellIcon />
+        <EmailIcon />
+      </BottomMenu> */}
+    </Container>
+  );
+};
+
+export default Main;
